835cc3d03556a32eae062c03c13f5c566626b91a
Kenaz <66073275+Kencel@users.noreply.github.com>
2025-07-17 22:09:04 +0800

    introduction 2/2

--- src/components/content/2-introduction.tsx
+++ src/components/content/2-introduction.tsx
@@ -1,10 +1,11 @@
+import { ca } from "date-fns/locale";
 import Image from "next/image";
 
 export default function Introduction() {
     return (
-        <div className="flex h-[calc(100vh-5rem)] items-center justify-center px-25">
-			<div className="block text-center m-auto w-1/2 h-ful overflow-auto [&>*]:py-5 [&>*]:px-10 [&>*]:text-justify [&_div]:text-lg [&_div]:font-dm_sans [&>*]:m-auto ">
-                <div className="">
+        <div className="block md:flex h-auto px-10 md:px-25">
+			<div className="block mx-auto w-1/2 overflow-auto [&>*]:py-5 [&>*]:px-10 [&>*]:text-justify [&_div]:text-lg [&_div]:font-dm_sans [&>*]:m-auto">
+                <div>
                     <h1 className="text-center sm:text-2xl md:text-4xl lg:text-5xl bg-[#E4E9F0] font-koulen p-5 rounded-2xl">
                         BIOWARFARE...?
                     </h1>
@@ -16,8 +17,23 @@ export default function Introduction() {
                     Casadavall has identified infectious disease as one of the three major existential threats to humanity. While the 1972 Biological Toxic Weapons Convention was written to limit the use of certain bioweapons, a few countries did not sign the treaty. There is also no assurance that the signed countries will fully adhere to the treaty. Bioterrorism methods have become easier to achieve as biotechnology becomes more accessible, easier to operate, and cheaper (Jansen et al., 2014). Meanwhile, these advanced technological capabilities allow the development of a biological weapon, including mechanisms that hasten the spread and increase the lethality of disease. Terrorists may potentially use these weapons to instill fear and inflict mass casualties.
                 </div>
             </div>
-            <div className="block text-center m-auto w-1/2 h-full">
-                
+            <div className="block mx-auto w-1/2 overflow-auto [&>*]:py-5 [&>*]:px-10 [&>*]:text-justify [&_div]:text-lg [&_div]:font-dm_sans [&>*]:m-auto">
+                <div>
+                    <Image
+                        src="/svgs/biosuits.svg"
+                        alt="biosuits"
+                        width={300}
+                        height={200}
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                    Source: nti.org
+                </div>
+                <div>
+                    While these biological attacks are alarming, hope is not lost. Despite the increasing availability of biotechnology, executing a successful bioterrorist attack still requires advanced skills. On the other hand, the same technologies have also led to improved scientific understanding of pathogens and the development of effective countermeasures. Innovation in surveillance technology has further enhanced early detection and response. While the threat of bioterrorism and biowarfare remains an ongoing concern, the likelihood of a large-scale, successful bioterrorist attack remains relatively low.
+                </div>
+                <div>
+                    Amid the growing accessibility of biotechnology, the dual-use dilemma poses uncertainty in its use for threat and protection in biological warfare and terrorism. This article discusses the mechanisms behind biological attack, biowarfare and bioterrorism strategies, historical and recent examples, how biotechnology is used both as a tool for warfare and defense, explore global and local strategies for prevention and preparedness, and look into the steps the Philippines took in response to such global threat.
+                </div>
             </div>
 		</div>
     );
