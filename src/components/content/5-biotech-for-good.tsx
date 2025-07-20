7d09c6c51c871b2f45a85992b545641f437eb7f0
Kenaz <66073275+Kencel@users.noreply.github.com>
2025-07-21 01:07:46 +0800

    biotech for good (no images yet)

--- src/components/content/5-biotech-for-good.tsx
+++ src/components/content/5-biotech-for-good.tsx
@@ -8,10 +8,267 @@ export default function BiotechForGood() {
 			</div>
 			<div className="[&>*]:[&_div]:font-dm_sans block h-auto md:flex md:px-25 [&>*]:mx-auto [&>*]:block [&>*]:overflow-auto [&>*]:[&>*]:px-10 [&>*]:[&>*]:py-5 [&>*]:[&>*]:text-justify [&>*]:md:w-1/2 [&>*]:[&_div]:text-lg">
 				<div>
+					<div>
+						Though biotechnology has been manipulated utilizing biological agents as a tool to cause
+						destruction on public safety and health, it has also been innovating solutions that can
+						counter these dangers.
+					</div>
+					<div>
+						According to Lim et al. (2005), public health officials recognize the need for rapid and
+						accurate detection as biothreat agents can be unpredictable. Reliable platforms must be
+						sensitive, specific and accurately detect a wide range of pathogens including modified
+						or unknown pathogens to also verify the occurrence of a bioterrorism event. Commercial
+						tests including biochemical tests, immunological detection devices, nucleic acid via
+						quantitative PCR and bioluminescence tests, in addition the new developing technologies
+						like aptamers, biochip, evanescent wave biosensors, cantilevers, and living cells are
+						methods to further improve detection speed and accuracy.
+					</div>
+					<div className="!pb-0">
+						To support this, Lim et al (2005) describe the Laboratory Response Network structured
+						four laboratory levels from A to D to ensure organized and effective handling of
+						biothreat agents.
+					</div>
+					<div className="my-0 py-0 [&>*]:!px-1 [&>*]:!py-1">
+						<div className="flex items-center gap-6">
+							<div className="flex h-15 w-15 flex-shrink-0 items-center justify-center rounded-2xl bg-gradient-to-b from-[#89F6CB] to-[#509077]">
+								<span className="font-koulen text-4xl font-bold text-black">A</span>
+							</div>
+							<p className="font-dm_sans italic">
+								often in standard clinic settings that serve as the first point of detection which
+								follow CDC standard guidelines on handling likely biothreat agents like Bacillus
+								anthracis, Yersinia pestis, and Francisella tularensis
+							</p>
+						</div>
+						<div className="flex items-center gap-6">
+							<div className="flex h-15 w-15 flex-shrink-0 items-center justify-center rounded-2xl bg-gradient-to-b from-[#89F6CB] to-[#509077]">
+								<span className="font-koulen text-4xl font-bold text-black">B</span>
+							</div>
+							<p className="font-dm_sans italic">
+								typically in public health laboratories that verifies the identity of the suspicious
+								organisms.
+							</p>
+						</div>
+						<div className="flex items-center gap-6">
+							<div className="flex h-15 w-15 flex-shrink-0 items-center justify-center rounded-2xl bg-gradient-to-b from-[#89F6CB] to-[#509077]">
+								<span className="font-koulen text-4xl font-bold text-black">C</span>
+							</div>
+							<p className="font-dm_sans italic">
+								More advanced testing to confirm the identity of species or strains using molecular
+								and typing methods.
+							</p>
+						</div>
+						<div className="flex items-center gap-6">
+							<div className="flex h-15 w-15 flex-shrink-0 items-center justify-center rounded-2xl bg-gradient-to-b from-[#89F6CB] to-[#509077]">
+								<span className="font-koulen text-4xl font-bold text-black">D</span>
+							</div>
+							<p className="font-dm_sans italic">
+								The highest level consists of facilities like CDC and US Army Medical Research
+								Institute of Infectious Diseases to handle advanced analysis on biothreat agents.
+							</p>
+						</div>
+					</div>
+					<div>
+						Lim et al. (2005), further introduces different detection and developing technologies
+						detecting and identifying these biothreat agents associated with bioterrorism and
+						biowarfare. From sample processing to detection technologies, these wide variations of
+						microbial agents are treated ensuring the minimization of the potential spread caused by
+						these pathogens.
+					</div>
 					<h2 className="font-koulen text-2xl">Preparation</h2>
+					<div>
+						Firstly, the study introduced the sample processing wherein biological agents are put in
+						a preparation before proceeding on the detection process. These include removal of other
+						substances as they can interfere during the detection of biothreat agents, ensuring
+						better accuracy and reliability of the detection technologies
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Sample matrix processing</h2>
+					<div>
+						A method that prepares the sample matrix by separating, concentrating and purifying.
+						Sample matrices can be on a variety of compositions the following are examples of
+						specific sample types: food, water, human specimens (blood, urine, stool specimens,
+						nasal and throat swab specimens), powders and soil, aerosols and surfaces. These sample
+						types are commonly tested for the presence of biothreat agents, due to the variety of
+						compositions, different sample preparation methods are required on each sample matrix
+						like centrifugation, filtration, dielectrophoresis, immunomagnetic separation and
+						nucleic acid extraction.
+					</div>
+					<h2 className="font-koulen text-2xl">Detecting technologies</h2>
+					<div>
+						Following the sample processing, the study proceeds to the different detection
+						technologies which vary on specificity and applicability. From manual to advanced, each
+						has their own strength and constraint on detecting and identifying the presence of
+						biological agents. May it vary on the speed, specificity, type of biological agent or
+						others may need further testing, understanding their usage helps to recognize their role
+						in reducing these threats impacted by bioterrorism and biowarfare.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Biochemical Tests</h2>
+					<div>
+						Biochemical tests are categorized in manual and automated. Manual Biochemical tests
+						where most machines used in local clinic and hospital laboratories are not built to
+						detect biothreat agents; thus, labs follow step-by-step processes to reduce potential
+						false results and act fast on removing biothreat agents. Using testing and presumptive
+						agent identification protocols to identify the following bacterias such as Bacillus
+						anthracis, Yersinia pestis. Francisella tularensis and Brucella spp. On the other hand,
+						automated biochemical tests which are mostly are not developed enough for identifying
+						and detecting biothreat agents however some new systems offers biothreat update package
+						using pattern recognition mainly Substrate utilization pattern (fingerprinting) which
+						uses VITEK and MicroLog, and Fatty acid profile which uses MDA Sherlock System.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Immunological detection devices</h2>
+					<div>
+						Immunoassay which is widely used in medical, pharmaceutical and food industries where it
+						detects biothreat agents through targeting antigen to trigger immune response. Andreotti
+						et al. made a comprehensive four core component of the immunoassay mainly{" "}
+						<i>
+							“(i) the antigen can be detected; (ii) the antibody or antiserum used for detection;
+							(iii) the method to separate bound antigen and antibody complexes from unbound
+							reactants; and (iv) the detection method.”
+						</i>
+						However it can only detect one analyte per assay, antibody quality limits the
+						specificity and PCR and other DNA-based sensitivity levels are higher than immunoassay.
+						However, there are platforms and devices used in immunoassays which are called “smart
+						ticket” technology: Solid support platforms like Luminex xMAP, BV technology,
+						Bio-detector, and DELFIA, and Lateral flow platforms.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">
+						Nucleic acid detection via quantitative PCR
+					</h2>
+					<div>
+						QPR combines PCR amplification with simultaneous detection using fluorescence dyes.
+						Specific detection format is a dual-labeled fluorogenic probe containing the fluorescent
+						dye and quencher dye to enable the change in fluorescence whereas non-specific detection
+						format is where fluorescence is bound to DNA when using DNA-intercalating dyes. The
+						non-specific format is more cost-efficient than specific and useful for optimizing PCR
+						conditions and checking primer specificity. Q-PCR is highly versatile in its
+						application, it can be useful for quantifying presence or absence, rapid confirmation
+						testing or monitor gene expression. However Q-PCR has its own limitation as the accuracy
+						and identification of bacteria is limited to the bias inherent in nucleic acid technique
+						and the presence of inhibitors can result in false negatives. Hence new advances are
+						made: Q-PCR thermocycler platforms and PCR reagent kits for biothreat agents.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Bioluminescence detection</h2>
+					<div>
+						Widely applied in clinical, food and environmental settings to detect bacterial
+						contamination. The most common example of bioluminescence detection is
+						Luciferin-luciferase reaction where the amount of ATP correlates to the biomass
+						proportionally. Since ATP is found in all living cells, the step by step process of
+						eliminating ATP contamination from non-target sources is required involving the use of
+						detergent to lyse the non-target cells so only the intended cells are monitored. Systems
+						like Profile-1 hand-system paired with Filtravette sample processing unit to detect
+						bioluminescence and remove non-bacterial ATP sources, these combined systems are also
+						effective on detecting contamination on meat carcasses, test biological aerosols and
+						identify Bacillus species spores in powder. However these methods lack specificity as it
+						only indicates the presence of ATP-producing microorganisms, requiring further testing
+						for serious threats.
+					</div>
 				</div>
 				<div>
-					<h2 className="font-koulen text-2xl">Detection technologies</h2>
+					<h2 className="font-koulen text-2xl">Developing technologies</h2>
+					<div>
+						Developing technologies can help in identifying and detecting biothreat agents, from
+						biochemical detection to chemical and physical detection, these advanced developing
+						sensor technologies are paired with transducers that can convert response into a signal
+						suitable for analysis.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Biochemical detection</h2>
+					<div>
+						Involves measuring products and enzymatic activity of enzymes during microbial
+						metabolism, though it tends to be less specific than antibody- and nucleic acid- based
+						methods as the product and enzyme is also present in other organisms. One example of
+						biochemical detection technology is electronic nose devices which employ transducers
+						like conducting polymers with chemicals that react with specific volatile organic
+						compounds or games. This sensor can also be used to detect multiple analytes, microbes
+						in food and infections and are used by researchers to identify volatile organic
+						compounds produced by bacteria or fungi. Though it can be rapid and sensitive, it
+						requires complex pattern recognition software to interpret data and lacks specificity
+						due to different microbes also producing similar volatile compounds. Conducting polymers
+						which are organic polymers conduct electricity to detect biologically produced chemicals
+						from microbial metabolism such as toxins, can be doped with enzymes, antibodies or other
+						biomolecules as it targets biological compounds like glucose, urea, and cholesterol
+						which are useful as biochemical detectors.
+					</div>
+					<div>
+						Some biosensors can detect bacterial toxins through the inhibition of enzymes such as
+						portable sensors for anatoxin-a, a toxin produced by a certain cyanobacteria that uses
+						electrochemical transducers to detect the activity of acetylcholinesterase though it can
+						be nonspecific since other compounds also inhibit the acetylcholinesterase activity. To
+						compensate, scientists have created mutated versions to increase sensitivity. Another
+						biosensor is β-Galactosidase activity with amperometric detection which is used to
+						quantify fecal coliform group of bacteria like E. coli and Klebsiella pneumoniae which
+						enhanced specificity through electrodes functionalized with antibodies or phage to lyse
+						bacteria followed by the amperometric detection of β-Galactosidase activity.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Tissue and cell-based detection</h2>
+					<div>
+						Used to identify biothreat agents using intrinsic response to detect toxins and
+						infectious foreign substances to a specific type of cell with a sensor containing action
+						potential signal, typically measured by electrode or optical detector. The cells may
+						originate from unicellular organisms or tissue types like nerve or heart cells and it
+						can be either primary or immortalized. Biosensors use cells from neurological and
+						cardiac tissues to detect harmful substance like biological threat agents, some examples
+						are cardiomyocytes — genetically engineered to respond selectively to a specific
+						functional activity, neuronal cells — an automated biosensor that responds to the
+						biological toxins tetrodotoxin and tityustoxin. However these biosensors are only
+						limited on expired toxins and are not capable to shape or sequence recognition like
+						antibody and nucleic acid sensors but can be advantageous when the threat is unknown.
+					</div>
+					<div>
+						Another example is B lymphocytes, a cell found in the human that displays surface
+						antibodies acting as pathogen receptors. Although undeveloped, researchers like Roder et
+						al. have enhanced the system through aequorin and pathogen-specific surface antibodies.
+						It may provide increased specificity to the cell-based system, it still faces the
+						antibody cross-reactivity problem and has the same storage and maintenance as the other
+						cell-based system. Chromatophores, found in cold-blooded animals that are responsible
+						for pigmentation and change color when animals are exposed to biologically active
+						substances like pesticides, neurotransmitters and bacterial toxins which can be observed
+						using a microscope or spectrophotometer. Chromatophores from fish do not grow thus it
+						does not need to be replaced frequently and has an ability to detect harmful chemicals,
+						biological toxins, toxin-producing pathogens and other biologically active agents.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Immunological detection</h2>
+					<div>
+						Antibody-based and similar affinity probes are technologies that utilize the immune
+						system specificity to target agents of interest through shape recognition technologies
+						that detect a wide range of biothreat agents. Research about immunological detection is
+						focused on improving sensitivity and specificity of antibodies however other types of
+						shape recognition technologies are under investigation. Using affinity probes can
+						potentially recover live organisms. The following are examples of antibodies, affinity
+						probes and detectors: Antibodies and fragments Aptamers and peptide ligands, Flow
+						cytometry, Biochip arrays, Surface plasmon resonance-based biosensors, Evanescent-wave
+						biosensors, Cantilever and acoustic wave and Quantum dots and upconverting phosphors.
+					</div>
+					<h2 className="font-dm_sans !pb-0 text-xl italic">Chemical and Physical Detection</h2>
+					<div>
+						Utilize biosensors that detect physical and chemical properties of the target analyte
+						like mass spectrometry, Raman spectrometry, and intrinsic fluorescence/luminescence. One
+						method of chemical and physical detection is the multi wavelength UV/visible
+						spectroscopic method which uses light-scattering and absorbing to differentiate
+						vegetative cells and spores but it is nonspecific and needs extensive sample preparation
+						on a non absorbing medium to produce a clean target. Mass spectrometry, particularly the
+						MALDI-TOF method, identifies the molecule by weight. It can detect mainly proteins from
+						bacteria and viruses, intact bacterial cells and spores like Bacillus thuringiensis and
+						Bacillus atrophaeus. MALDI-TOF method is a fast system that works only with small sample
+						volume, requires little sample preparation and is capable of identifying all the types
+						of biological agents. Despite being the most used approach, it requires a highly
+						concentrated sample, needs to develop complex fingerprints on every target and lacks
+						specificity in mixed targets. FLow field-flow fractionation can improve MALDI-TOF/mass
+						spectrometry to help in separating articles on the mixed targets.
+					</div>
+					<div>
+						Another chemical and physical detection method is the Electrospray ionization Fourier
+						transform ion cyclotron resonance (EIS-FTICR) that is used to analyze alleles such as
+						variable-number tandem repeats and single-nucleotide polymorphisms but require DNA base
+						pairs shorter than 200. Surface-enhanced Raman scattering (SERS) is another method that
+						studies nucleic acids, pathogens, and toxins and requires multiple fingerprints on each
+						target analyte. Methods by Bell et al. (adding sodium sulfate and thiosulfate) and Zhang
+						et al. (used silver firm) are used to detect dipicolinic acid and improve sensitivity.
+						SERS method can differentiate between viable and nonviable organisms and specific toxins
+						in mixed samples and incorporated a biochip platform that used antibodies to capture
+						target analytes that detect listeria and legionella species, bacillus species spores,
+						cryptosporidium parvum and cryptosporidium meleagridis. The last method mentioned is the
+						Near-infrared SERS method that identifies different spores like Bacillus
+						stearothermophilus, strains like Escherichia coli and urinary tract infections.
+					</div>
 				</div>
 			</div>
 		</div>
